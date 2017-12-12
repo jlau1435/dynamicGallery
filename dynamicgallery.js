a1d7ff7e8ea1e6d6b3aaeac15b4835caae3be871
Biggi Schattinger <biggischatti@users.noreply.github.com>
2017-12-11 16:08:17 -0800

    updates 12/11/17 with Slider

--- dynamicgallery.js
+++ dynamicgallery.js
@@ -1,3 +1,4 @@
+
 var pictureArray = ["pic1.jpg", "pic2.jpeg", "pic3.jpeg", "pic4.jpg", "pic5.jpg", "pic6.jpeg"];
 
 var altName = [
@@ -10,20 +11,69 @@ var altName = [
 
 var length = pictureArray.length;
 
-for (var i = 0; i < length; i = i+1) {
+
+;
+ 
+    
+ // SLIDER
+    
+var sliderIndex = 0;
+    
+var currentSliderImage = pictureArray[sliderIndex];
+    
+    
+    var currentSlider = "<img src=\"Images/" + pictureArray[sliderIndex] + "\">";
+    
+    $("#currentPicture").append(currentSlider);
+    
+    
+    
+    
+    
+    
+    
+// DYNAMIC GALLERY
+  
+
+for(var i = 0; i < length; i = i+1) {
     
   var picture = "<img src=\"Images/" + pictureArray[i] + " \"  alt=\" " + altName[i] + " \"  height=\"300\" width=\"400\">";
     
-   var linkImage ="<a href=\" images/" + pictureArray[i] +" \">" + picture + "</a>";
-  
-    var pictureListItem = "<li>" + linkImage + "</li>";
+var linkImage = picture;
+var pictureListItem = "<li id=\"image-" + i + "\">" + linkImage + "</li>";
     
-    console.log (picture);
-    
-    $("body").css("background-color", "gray");
-
-    
- $("#mainbox").append(pictureListItem);
+$("body").css("background-color", "gray") 
+   $("#mainbox").append(pictureListItem);
     
 console.log (picture);
+}
+
+for (var i =0; i< pictureArray.length; i = i + 1) {
+    
+   
+   $("#image-" + i + " img").click( function(e) {
+        
+       
+var currentImage = $(this).attr("src");
+       
+var currentAltName = $(this).attr("alt");
+       
+      
+       $("#lightbox").css("display", "block");
+       
+       console.log(currentImage);
+        
+       
+       $("#lightbox").html("<img src=\" " + currentImage + " \"  alt=\" " + currentAltName + " \"  height=\"600\" width=\"800\">"
+        );
+        
+        $("#lightbox img").click (function(e){
+            
+            
+            $("#lightbox").css("display", "none");
+            
+        } );
+        
+    }  );  
+   
 }
---
+++ dynamicgallery2.js
@@ -0,0 +1,35 @@
+var pictureArray = ["pic1.jpg", "pic2.jpeg", "pic3.jpeg", "pic4.jpg", "pic5.jpg", "pic6.jpeg"];
+
+var altName = [
+    "Railroad tracks", 
+    "Dock to the sunset", 
+    "Spring rain", 
+    "Sun rais in the forest", 
+    "Poppy field sunset", 
+    "Daisies in the meadow"];
+
+var length = pictureArray.length;
+
+for (var i = 0; i < length; i = i+1) {
+    
+  var picture = "<img src=\"Images/" + pictureArray[i] + " \"  alt=\" " + altName[i] + " \"  height=\"300\" width=\"400\">";
+    
+   var linkImage ="<a href=\" images/" + pictureArray[i] +" \">" + picture + "</a>";
+  
+    var pictureListItem = "<li id=\"image-" + i + "\">" + linkImage + "</li>";
+    
+    console.log(picture);
+    
+    $("body").css("background-color", "gray");
+
+    
+ $("#mainbox").append(pictureListItem);
+    
+console.log (picture);
+}
+
+    $("#showdiv").click( function(e) {
+        
+        console.log("pressed a button!");
+        $("#lightbox").css("display", "block");
+    }  );
